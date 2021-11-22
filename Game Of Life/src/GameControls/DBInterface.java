98a59e8c9fd3a4d4f04c01ff576aec1c9003c513
SharjeelJathol <66735435+SharjeelJathol@users.noreply.github.com>
2021-11-22 22:14:46 +0500

    Creating Interfaces to be implemented

---
+++ Game Of Life/src/GameControls/DBInterface.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GameControls;
+import State.State;
+/**
+ *
+ * @author Shurahbeel Ahmad
+ */
+public interface DBInterface {
+    public State loadState();
+    public void saveState(State state);
+    public void deleteState();
+    
+    
+}
