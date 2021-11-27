792cdbd1897e40ea09df21a5b3487782b47f182b
SharjeelJathol <66735435+SharjeelJathol@users.noreply.github.com>
2021-11-27 17:48:12 +0500

    Update GUI_Implementation.java

--- Game Of Life/src/GUI/GUI_Implementation.java
+++ Game Of Life/src/GUI/GUI_Implementation.java
@@ -12,24 +12,39 @@ import GameControls.GUI_Interface;
  * @author Shurahbeel Ahmad
  */
 public class GUI_Implementation  implements GUI_Interface{
-
+    State currentState=new State(7, 7);
+    currentState.setInstance(2, 3, true);
+    currentState.setInstance(3, 3, true);
+    currentState.setInstance(4, 3, true);
     @Override
     public State getGrid()
     {
-        State state=new State(10, 10);
-        state.setInstance(3, 3, true);
-        state.setInstance(3, 4, true);
-        state.setInstance(3, 5, true);
-        //state.print();
+        //State state=new State(10, 10);
+        
+        //printGrid(currentState);
         System.out.println("Printed the Current Grid");
-        return state;
+        return currentState;
     }
+    
+    public void setGrid(State state)
+    {
+        currentState=state;
+        state=null;
+        printGrid(currentState);
+    }
+    
     public void printGrid(State state)
     {
         for(int i=0; i<state.getRows();i++)
      {
          for(int j=0; j<state.getCols();j++)
-             System.out.print(state.getInstance(i, j)+"-");
+         {
+             //System.out.print(state.getInstance(i, j)+"-");
+             if(state.getInstance(i, j)==true)
+                 System.out.print(1+"-");
+             else
+                 System.out.print(0+"-");
+         }
          System.out.println("");
      }
         System.out.println("Printed the New Grid");
